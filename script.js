53c57e5fb79874c023f17633e394ca35a13e68f7
Tamunowari Sotom <142884295+Sotom-tam@users.noreply.github.com>
2024-05-08 12:03:19 +0100

    Progress in Styling and script
    I made changes to CSS, For reponsiveness, and annded a script.js to sid reponsiveness

---
+++ script.js
@@ -0,0 +1,7 @@
+let x=window.innerHeight
+let y=window.innerWidth
+// let ans=(22/100)*y
+// // if(x>=)
+// ans=String(ans)
+// let container=document.getElementById("con");
+// console.log(container.style.width=`${ans}`+"px")
